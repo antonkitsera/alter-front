537e5d68861eddaeeb3cfa97f81a92634a007ead
Anton Kitsera <antonkitsera@gmail.com>
2020-02-29 20:02:22 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
-          include: `${__dirname}/src/assets/`
+          include: /assets/
         }
       }
     },
